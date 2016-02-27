618349716127c8321ef525692bb4e3acd0e395bb
Brendan <brendan@burkesoftware.com>
2016-02-27 08:22:27 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package michelle-y-burke
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'michelle-y-burke' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'michelle-y-burke' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'michelle-y-burke' ), 'michelle-y-burke', '<a href="http://burkesoftware.com/" rel="designer">Burke Software and Consulting</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
